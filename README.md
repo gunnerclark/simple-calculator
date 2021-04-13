77e0ad048a3bc94adfd52ce20dc1b7c318dc964a
inept <63522725+theineptdev@users.noreply.github.com>
2021-04-13 12:25:23 -0500

    completed

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
-# simple-calculator
-A simple, web-based, calculator
+# [simple calculator](http://theinept.dev)
+
+A simple, web-based, calculator. Built by following [Web Dev Simplified](https://www.youtube.com/channel/UCFbNIlppjAuEX4znoulh0Cw)'s [JavaScript calculator tutorial](https://www.youtube.com/watch?v=j59qQ7YWLxw&ab_channel=WebDevSimplified). 
+
+Created to refresh my JavaScript skillset while also familiarizing myself with how others use the language.
+
+# Screenshots
+
+<p align="center">
+  <img src="screenshots/clearCalcEx.png" width="49%">
+  <img src="screenshots/useCalcEx.png" width="49%">
+</p>
+
+# Technology
+
+Created with JavaScript, HTML, and CSS.
---
+++ script.js
@@ -0,0 +1,140 @@
+class Calculator {
+    constructor(previousOperandTextElement, currentOperandTextElement) {
+        // allows us to set the text elements within class
+        this.previousOperandTextElement = previousOperandTextElement;
+        this.currentOperandTextElement = currentOperandTextElement;
+        this.clear();
+    }
+
+    // clear out the different variables
+    clear() {
+        this.currentOperand = '';
+        this.previousOperand = '';
+        this.operation = undefined;
+    }
+
+    // remove a single number
+    delete() {
+        this.currentOperand = this.currentOperand.toString().slice(0, -1);
+    }
+
+    // appending a number to the screen
+    appendNumber(number) {
+        if (number === '.' && this.currentOperand.includes('.')) {
+            return
+        }
+        this.currentOperand = this.currentOperand.toString() + number.toString();
+    }
+
+    // sets selected operation
+    chooseOperation(operation) {
+        if (this.currentOperand === '') {
+            return
+        }
+        if (this.previousOperand !== '') {
+            this.compute();
+        }
+        this.operation = operation;
+        this.previousOperand = this.currentOperand;
+        this.currentOperand = '';
+    }
+
+    // take values and compute a single value to display
+    compute() {
+        let computation;
+        const prev = parseFloat(this.previousOperand);
+        const current = parseFloat(this.currentOperand);
+        if (isNaN(prev) || isNaN(current)) {
+            return
+        }
+        switch (this.operation) {
+            case '+':
+                computation = prev + current;
+                break;
+            case '-':
+                computation = prev - current;
+                break;
+            case '•':
+                computation = prev * current;
+                break;
+            case '÷':
+                computation = prev / current;
+                break;
+            default:
+                return
+        }
+        this.currentOperand = computation;
+        this.operation = undefined;
+        this.previousOperand = '';
+    }
+
+    // add commas and other styling to display text
+    getDisplayNumber(number) {
+        // display decimal when selected & zeros after pressing decimal
+        const stringNumber = number.toString();
+        const integerDigits = parseFloat(stringNumber.split('.')[0]);
+        const decimalDigits = stringNumber.split('.')[1];
+        let integerDisplay;
+        if (isNaN(integerDigits)) {
+            integerDisplay = '';
+        } else {
+            integerDisplay = integerDigits.toLocaleString('en', {
+                maximumFractionDigits: 0
+            })
+        }
+        if (decimalDigits != null) {
+            return `${integerDisplay}.${decimalDigits}`
+        } else {
+            return integerDisplay;
+        }
+    }
+
+    // update the values in the output
+    updateDisplay() {
+        this.currentOperandTextElement.innerText = this.getDisplayNumber(this.currentOperand);
+        if (this.operation != null) {
+            this.previousOperandTextElement.innerText = `${this.getDisplayNumber(this.previousOperand)} ${this.operation}`
+        } else {
+            this.previousOperandTextElement.innerText = '';
+        }
+    }
+}
+
+const numberButtons = document.querySelectorAll('[data-number]');
+const operationButtons = document.querySelectorAll('[data-operation]');
+const equalsButton = document.querySelector('[data-equals]');
+const deleteButton = document.querySelector('[data-delete]');
+const allClearButton = document.querySelector('[data-all-clear]');
+const previousOperandTextElement = document.querySelector('[data-previous-operand]');
+const currentOperandTextElement = document.querySelector('[data-current-operand]');
+
+const calculator = new Calculator(previousOperandTextElement, currentOperandTextElement);
+
+numberButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        calculator.appendNumber(button.innerText);
+        calculator.updateDisplay();
+    })
+})
+
+operationButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        calculator.chooseOperation(button.innerText);
+        calculator.updateDisplay();
+    })
+})
+
+equalsButton.addEventListener('click', () => {
+    calculator.compute();
+    calculator.updateDisplay();
+})
+
+allClearButton.addEventListener('click', () => {
+    calculator.clear();
+    calculator.updateDisplay();
+})
+
+deleteButton.addEventListener('click', () => {
+    calculator.delete();
+    calculator.updateDisplay();
+})
